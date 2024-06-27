32b23e9c953d9024d0da2a13ebfe3b154acbca14
moe <catmonky72@gmail.com>
2024-06-27 11:55:12 -0400

    test commit

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Rusn administrative tasks. """
+    """Rusn administrative  tasks. """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voting_project.settings')
     try:
         from django.core.management import execute_from_command_line
