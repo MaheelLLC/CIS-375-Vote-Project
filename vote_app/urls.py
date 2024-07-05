34768bd7c82c238e7e1e73bdc522c8f1051b6733
Mouhamad Ismail <mhismail@umich.edu>
2024-07-05 14:40:46 -0400

    Fixed the register Made he login page point you to the register page Added more rules for registering (email, username, and password)

--- vote_app/urls.py
+++ vote_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
--- vote_app/views.py
+++ vote_app/views.py
@@ -46,9 +46,8 @@ def register_view(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
+            login(request, user)
+            return redirect('home')
     else:
         form = CustomUserCreationForm()
     return render(request, 'vote_app/registerPage.html', {'form': form})
