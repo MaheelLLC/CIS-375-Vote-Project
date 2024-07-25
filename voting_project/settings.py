ce2d9e595cb70159897c158e17ba875c6c4694e5
Mouhamad Ismail <mhismail@umich.edu>
2024-07-25 12:18:29 -0400

    added an icon to almost all the pages from the chrome/edge view

--- voting_project/settings.py
+++ voting_project/settings.py
@@ -119,9 +119,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'vote_app', 'templates'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
